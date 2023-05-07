ddea963f26c56fe90c08533ae5565f811abdcc1a
Olayanju-1234 <horlarhyanjuhjoseph@gmail.com>
2023-05-07 12:47:27 +0100

    new update

--- app.js
+++ app.js
@@ -1,22 +1,20 @@
 const express = require('express');
-const webPush = require('web-push');
 const bodyParser = require('body-parser');
 const path = require('path');
-
-require('dotenv')
-
-const app = express();
 
 // Route
 const subscribeRoute = require('./routes/subscribe');
 
+const app = express();
+
+// set static path
+app.use(express.static(path.join(__dirname, 'client')));
+
 // body parser middleware
 app.use(bodyParser.json());
 
-
 // route
 app.use('/', subscribeRoute);
-
 
 // port
 const port = process.env.PORT || 5000;
